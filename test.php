8164942da3f6e32c8a118993540b475c0458a0c8
vishvaa <vishvaalogeswaran@gmail.com>
2023-09-05 19:47:28 +0800

    Add/Testing headers and footers

--- test.php
+++ test.php
@@ -1,18 +1,57 @@
-<!DOCTYPE html>
-<!--
-Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
--->
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns = "http://www.w3.org/1999/xhtml" xml:lang = "en">
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <meta http-equiv = "content-type" content = "text/html; charset = utf-8" />
+        <title> Test homepage </title>
+        <link rel = "stylesheet" type = "text/css" href = "css/test.css">
     </head>
     <body>
-        <p>testing git</p>
+        
+        <header>
             
-        <?php
-        // put your code here
-        ?>
+            <div class ="hero">
+                <nav>
+                    <h2 class="logo">John Doe</h2>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                    </ul>
+                </nav>
+            </div>
+            
+        </header>
+        
+        <div class= "content">
+            
+            
+            
+        </div>
+        
+        <footer>
+        <div class = "footerContainer">
+            <div class = "smLogo">
+                <a href = ""><i class = "fa-brands fa-facebook"></i></a>
+                <a href = ""><i class = "fa-brands fa-instagram"></i></a>
+                <a href = ""><i class = "fa-brands fa-twitter"></i></a>
+            </div>
+            <div class = "footerLinks">
+                <ul>
+                    <li><a href = "Home">Home</a></li>
+                    <li><a href = "Account">Account</a></li>
+                    <li><a href = "Notifications">Notifications</a></li>
+                    <li><a href = "About Us">About Us</a></li>
+                    <li><a href = "Contact Us">Contact Us</a></li>
+                </ul>
+            </div>
+            
+        </div>
+            <div class = "footerBottom">
+                <p>Copyright &copy;2023; Designed by <span class = "designer">Vishvaa</span></p>
+            </div>
+        </footer>
+        
     </body>
 </html>
